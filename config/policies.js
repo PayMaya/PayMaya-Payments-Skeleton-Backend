1431ef78a2d520e92c4998d6746a449261e892fd
Elijah Joshua Cayabyab <ebcayabyab@voyagerinnovation.com>
2016-02-24 09:42:56 +0800

    Add policy for payments endpoint

--- config/policies.js
+++ config/policies.js
@@ -32,6 +32,9 @@ module.exports.policies = {
   },
   'UserController' : {
     '*': ['passport','bearerAuth']
+  },
+  'v1/PaymentsController' : {
+    '*': ['passport','bearerAuth']
   }
 
   // '*': true,
